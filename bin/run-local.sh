caf442907b9f4515bad70a7ac787cd75523972e4
Ilya Dashevsky <idashevsky>
2021-04-11 11:57:02 +0300

    Support for php 8

--- bin/run-local.sh
+++ bin/run-local.sh
@@ -2,6 +2,13 @@
 
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 
+PORT=9090
+HOST=localhost
+
+
+
+xdg-open "http://$HOST:$PORT"
+
 
 cd "$APP_DIR"
 
@@ -14,7 +21,7 @@ echo "Starting..."
 while :
 do
 	DATA_ROOT="$DATA_ROOT" \
-    php -S localhost:9090 -t "public"
+    php -S $HOST:$PORT -t "public"
 
     echo "Restarting... Press [CTRL+C] to abort"
 	sleep 1
